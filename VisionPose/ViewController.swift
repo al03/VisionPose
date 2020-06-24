9518d5a93e1f5e59587b7cf31bf2d0bc56c4b1a4
al03 <maokaiyin@gmail.com>
2020-06-24 16:49:37 +0800

    Initial Commit

---
+++ VisionPose/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VisionPose
+//
+//  Created by Albert on 6/24/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
